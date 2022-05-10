b0c06604e8f3a60e142539e6d3ff11cd07063bff
andrius6789 <you@example.com>
2022-05-10 17:46:10 -0400

    rangos

--- Main.py
+++ Main.py
@@ -4,3 +4,19 @@ from gurobipy import * #se importa todo de la biblioteca
 m = Model('riego en predios agricolas') #creacion del modelo con un nombre y "m" es el objeto de la clase modelo
 print(type(m))
 
+#valores limites de los conjuntos. SUJETOS A CAMBIOS
+D = 28
+I = 5
+H = 100
+T = 24 #no cambiar este
+J = 3
+
+#rangos
+d_ = range(1, D + 1) #dias
+i_ = range(1, I + 1) #tipo de cultivo
+h_ = range(1, H + 1) #cuadrante
+t_ = range(1, T + 1) #horas
+j_ = range(1, J + 1) #metodos de riego
+
+#conjuntos
+    
