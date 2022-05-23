c128acb9e59375729ad721543a8bf0dd98efb7f0
Benjamín Lillo <benjalso99@gmail.com>
2022-05-22 21:17:58 -0400

    final

--- Main.py
+++ Main.py
@@ -1,100 +1,170 @@
-import random
-from gurobipy import * #se importa todo de la biblioteca
-from gurobipy import Model, GRB, quicksum # se agrega Model, quicksum
-#m.write("riego en predios agricolas.lp")
+import csv
+import os
+from gurobipy import GRB, Model, quicksum
 
-m = Model('riego en predios agricolas') #creacion del modelo con un nombre y "m" es el objeto de la clase modelo
-
-#valores limites de los conjuntos. SUJETOS A CAMBIOS -> '' hacerlos input ''
-D = 28
-I = 5
-H = 100
-T = 24 #no cambiar este
+I_ = 2
+T = 24
+H = 2
+D = 135
 J = 3
-Amin, Amax = 1,5
 
-#rangos
-d_ = range(1, D + 1) #dias
-i_ = range(1, I + 1) #tipo de cultivo
-h_ = range(1, H + 1) #cuadrante
-t_ = range(1, T + 1) #horas
-j_ = range(1, J + 1) #metodos de riego
+#instanciar moddelo
+model = Model()
 
-#conjuntos/parametros
-# Ne = [{i,h,d} for i in i_ for h in h_ for d in d_] #hay que darle valor 1 o 0
+# conjuntos
 
-Ne = {(i, h, d): random.randint(0, 1) for i in i_ for h in h_ for d in d_} # cada Ne_ihd se instancia como 0 o 1 random
+i_ = range(1, I_ + 1)
+t_ = range(1, T + 1)
+h_ = range(1, H + 1)
+d_ = range(1, D + 1)
+j_ = range(1, J + 1)
 
-# A = [{h} for h in h_] #como le asociamos valores reales de m^2 a cada area para todo h?, son valores iguales para todo h?
+# parámetros (todos como diccionarios)
 
-A = {h: random.uniform(Amin, Amax) for h in h_} # cada A_h es un valor random entre 1 y 5 m2
+Ne = {(i, h, d): 0 for i in i_ for h in h_ for d in d_}
+# se plantan ambos cultivos el primer dia y despues de esto nada mas
+Ne.update({(1, 1, 1): 1})
+Ne.update({(2, 2, 1): 1})
 
-DA = [{d} for d in d_] #misma pregunta re arriba, es un valor constante no? podria quedar DA = input("cuales son sus derechos de agua en metros cubicos")
-ETo = [{d} for d in d_]
-P = input("cual es su presupuesto en pesos?: ") #para todos los dias d
-E = [{j,t} for j in j_ for t in t_] #como asociar valores no randomizados a cada par de puntos? quizas son valores entonces habria que especificar la eficiencia por hora
-PP = [{d} for d in d_] #como ingresan estos valores al programa?
-V = [{i,j} for i in i_ for j in j_] #hacer binario. Se me ocurre que se podria tratar como variable y crear como GRB.BINARY
-C = [{h,t} for h in h_ for t in t_]
-M = [{h,j} for h in h_ for j in j_]
-Ce = input('Cual es la capacidad del estanque en Litros: ')
-alfa = input('agua almacenada al inicio, en Litros: ')
-T1 = [{i} for i in i_]
-T2 = [{i} for i in i_]
-T3 = [{i} for i in i_]
-T4 = [{i} for i in i_]
-Tt = [T1 + T2 + T3 + T4 for i in i_] #revisar que este bien definido
-#print(type(T1[0]))
-Kc1 = [{i} for i in i_]
-Kc2 = [{i} for i in i_]
-Kc3 = [{i} for i in i_]
+A = {1: 10000, 2: 10000} # 2 cuadrantes, ambos de 10.000 m2
 
-Z = m.addVars(h_,t_,d_,j_)
-X = m.addVars(h_,t_,d_)
-Y = m.addVars(t_,d_)
-Zr =m.addVars(h_,t_,d_)
-W = m.addVars(h_,j_,i_, vtype= GRB.BINARY)
-II =m.addVars(t_,d_) #Este es I pero ya existe un valor I para los rangos
+DA = {d: 864000 for d in d_} # derechos de agua para cada dia d
+
+with open(os.path.join('Data', 'Datos_resumidos.csv'), mode='r') as file:
+    reader = csv.reader(file, delimiter=';')
+    next(reader, None)
+    next(reader, None) # saltarse headers
+    Pp = dict()
+    ETo = dict()
+    for col in reader:
+        Pp.update({int(col[2]):float(col[3].replace(',', '.'))}) # Pp de cada dia en mm leido del csv del excel con los datos.
+        ETo.update({int(col[5]):float(col[6].replace(',', '.'))}) # ETo de cada dia en mm leido del csv del excel con los datos.
+
+P = 80000000 # dato arbitrario elegido en informe E2
+
+E = {1: 0.8, 2: 0.7, 3: 0.2}
+
+V = {(i, j): 1 for i in i_ for j in j_}
+
+C = {(h, t): 0.11 for h in h_ for t in t_} # clp
+
+M = {(1, 1): 4750, (2, 1): 4750, (1, 2): 3250, (2, 2): 3250, (1, 3): 1750, (2, 3): 1750} # clp
+
+Ce = 100000 # en L
+
+alpha = 50000 # en L
+
+# tiempos en dias
+T1 = {1: 20, 2: 30}
+
+T2 = {1: 20, 2: 40}
+
+T3 = {1: 15, 2: 40}
+
+T4 = {1: 10, 2: 25}
+
+Tt = {1: 75, 2: 135}
+
+Kc1 = {1: 0.7, 2:0.6}
+
+Kc2 = {1: 1, 2: 1.15}
+
+Kc3 =  {1: 0.95, 2: 0.8}
+
+Mg = 1000000000000000 # M grande
+
+# variables
+
+Z = model.addVars(h_, t_, d_, j_, vtype = GRB.CONTINUOUS, name='Z_htdj')
+
+X = model.addVars(h_, t_, d_, vtype = GRB.CONTINUOUS, name='X_htd')
+
+Y = model.addVars(t_, d_, vtype = GRB.CONTINUOUS, name='Y_td')
+
+Zr = model.addVars(h_, t_, d_, vtype = GRB.CONTINUOUS, name='Zr_htdj')
+
+W = model.addVars(h_, j_, i_, vtype = GRB.BINARY, name='W_hji')
+
+I = model.addVars(t_, d_, vtype = GRB.CONTINUOUS, name='I_td')
 
 
-#Restriccion i) 
-## Como agregas una igualdad de valores para un punto del conjunto especifico??
-###dice como comentario que sobra X, habria que eliminar la suma de Xhtd y listo considerando que se soluciona "##"
-# m.addConstrs(((II[t-1,d]) + sum(X[h,t,d] for h in h_) + Y[t,d] == quicksum(Z[h,t,d,j] + II[t,d] for j in j_ for h in h_) + II[t,d]) for t in range (2,T+1) for d in d_ )
+model.update()
 
-#Restriccion ii)
-#m.addConstrs(quicksum(Z[h,t,d,j]*E[j,t] for t in t_ for j in j_) + Pp[d] * A[h] >= sum(Zr[h,t,d]) for h in h_ for d in d_)
-#la naturaleza de algun valor esta definido como tuplas (puntos) y no como valores por lo que no genera una restriccion valida
+# restricciones
 
-#Restriccion iii)
-#m.addConstrs(quicksum(Zr[h,t,d] for d in range(q + 1,q+T1[i]+1) for t in t_) >= quicksum(Kc1[i]*ETo[q]) - 10000*(1-Ne[i,h,q] for d in range(q+1,q+T1[i]+1) ) for j in j_ for h in h_ for i in i_ for q in range(1,D-Tt[i]+1) )
-#m.addConstrs(quicksum(Zr[h,t,d] for d in range(q + T1[i]+1,q+T1[i]+T2[i]+1) for t in t_) >= quicksum(0.5*(Kc1[i]+Kc2[i])*ETo[q]) - 10000*(1-Ne[i,h,q] for d in range(q+1,q+T1[i]+1) ) for j in j_ for h in h_ for i in i_ for q in range(1,D-Tt[i]+1) )
-#m.addConstrs(quicksum(Zr[h,t,d] for d in range(q + T1[i]+T2[i]+1,q+T1[i]+T2[i]+T3[i]+1) for t in t_) >= quicksum(Kc2[i]*ETo[q]) - 10000*(1-Ne[i,h,q] for d in range(q+1,q+T1[i]+1) ) for j in j_ for h in h_ for i in i_ for q in range(1,D-Tt[i]+1) )
-#m.addConstrs(quicksum(Zr[h,t,d] for d in range(q + T1[i]+T2[i]+T3[i]+1,q+Tt[i]+1) for t in t_) >= quicksum(0.5*(Kc2[i]+Kc3)*ETo[q]) - 10000*(1-Ne[i,h,q] for d in range(q+1,q+T1[i]+1) ) for j in j_ for h in h_ for i in i_ for q in range(1,D-Tt[i]+1) )
+# i
+model.addConstrs(((I[t - 1, d]) + Y[t, d] == quicksum(Z[h, t, d, j] + I[t, d] for j in j_ for h in h_) for t in range(2, T + 1) for d in d_), name='R1')
 
-#Restriccion iv)
-# m.addConstrs(P >= quicksum(C[h,t] for j in j_ for d in d_ for t in t_ for h in h_) + quicksum(W[h,j] * M[h,j] for j in j_ for h in h_))
-#lo mismo de ii)
-## me parece que esta bien definida, el problema es la manera en las que estan construidos los parametros
+model.addConstrs((I[1, d] == I[24, d - 1] for d in range(2, D + 1)), name='R2')
 
-#Restriccion v)
+model.addConstr((I[1, 1] == alpha), name='R3')
 
-#m.addConstrs( sum(W[j,h] for j in j_) == 1 for h in h_ )
-#parece que lo mismo de ii y iv
+# ii
 
-#m.addConstrs(M * W[h,j,i] >= quicksum(Z[h,t,d,j] for d in d_ for t in t_) for h in h_ for j in j_ for i in i_)
-#agregue el para todo i de i_ porque no estaba considerado
+model.addConstrs((quicksum(Z[h, t, d, j] * E[j] for t in t_ for j in j_) + Pp[d] * A[h] >= quicksum(Zr[h, t, d] for t in t_) for h in h_ for d in d_), name='R4')
 
-#m.addConstrs( V[i,j] >= W[h,j,i] for h in h_ for j in j_ for i in i_)
-#hay que definir bien las cosas, siguen siendo tuplas
+# iii
+model.addConstrs((quicksum(Zr[h, t, d] for d in range(q + 1, q + T1[i] + 1) for t in t_) >= quicksum(Kc1[i] * ETo[q] - Mg * (1 - Ne[i, h, q]) for d in range(q + 1, q + T1[i] + 1)) for i in i_ for j in j_ for h in h_ for q in range(1, D - Tt[i] + 1)), name='R5')
 
-#Restriccion vi)
+model.addConstrs((quicksum(Zr[h, t, d] for d in range(q + T1[i] + 1, q + T1[i] + T2[i] + 1) for t in t_) >= quicksum((((Kc1[i] + Kc2[i]) * ETo[q]) / 2) - Mg * (1 - Ne[i, h, q]) for d in range(q + T1[i] + 1, q + T1[i] + T2[i] + 1)) for i in i_ for j in j_ for h in h_ for q in range(1, D - Tt[i] + 1) ), name='R6')
 
-#m.addConstrs( quicksum(X[h,t,d] + Y[t,d] for t in t_ for h in h_) <= DA[d] for d in d_)
-##corregir naturaleza 
+model.addConstrs((quicksum(Zr[h, t, d] for d in range(q + T1[i] + T2[i] + 1, q + T1[i] + T2[i] + T3[i] + 1) for t in t_) >= quicksum(((Kc2[i] * ETo[q]) / 2) - Mg * (1 - Ne[i, h, q]) for d in range(q + T1[i] + T2[i] + 1, q + T1[i] + T2[i] + T3[i] + 1)) for i in i_ for j in j_ for h in h_ for q in range(1, D - Tt[i] + 1) ), name='R7')
 
-#Restriccion vii)
-#m.addConstrs( II[t,d] <= C[e] for d in d_ for t in t_) cual es el rango de e???
+model.addConstrs((quicksum(Zr[h, t, d] for d in range(q + T1[i] + T2[i] + T3[i] + 1, q + Tt[i] + 1) for t in t_) >= quicksum((((Kc2[i] + Kc3[i]) * ETo[q]) / 2) - Mg * (1 - Ne[i, h, q]) for d in range(q + T1[i] + T2[i] + T3[i] + 1, q + Tt[i] + 1)) for i in i_ for j in j_ for h in h_ for q in range(1, D - Tt[i] + 1)), name='R8')
 
-funcion_objetivo = quicksum(Z[h,t,d,j] for d in d_ for t in t_ for h in h_ for j in j_)
-m.setObjetive(funcion_objetivo, GRB.MINIMIZE)
+print('for==========')
+for i in i_:
+    
+    for q in range(1, D - Tt[i] + 1):
+        for d in range(q + T1[i] + T2[i] + T3[i] + 1, q + Tt[i] + 1):
+            if d >= 135:
+                print(d)
+    
+# iv
+
+model.addConstr((P >= quicksum(Z[h, t, d, j] * C[h, t] for j in j_ for d in d_ for t in t_ for h in h_) + quicksum(W[h, j, i] * A[h] * M[h, j] for j in j_ for h in h_ for i in i_)), name='R9')
+
+# v
+
+model.addConstrs((quicksum(W[h, j, i] for j in j_) == 1 for  h in h_ for i in i_), name='R10')
+
+model.addConstrs((Mg * W[h, j, i] >= quicksum(Z[h, t, d, j] for d in d_ for t in t_) for h in h_ for j in j_ for i in i_), name='R11')
+
+model.addConstrs((V[i, j] >= W[h, j, i] for h in h_ for j in j_ for i in i_), name='R12')
+
+# vi
+
+model.addConstrs((quicksum(X[h, t, d] + Y[t, d] for t in t_ for h in h_) <= DA[d] for d in d_), name='R13')
+
+# vii
+
+model.addConstrs((I[t, d] <= Ce for d in d_ for t in t_), name='R14')
+
+# Naturaleza de las variables: se hace sola al instanciar las variables
+
+# funcion objetivo
+
+model.setObjective(quicksum(Z[h, t, d, j] for d in d_ for t in t_ for h in h_ for j in j_), GRB.MINIMIZE)
+
+# Optimiza tu problema
+
+model.optimize()
+
+vo = model.ObjVal
+
+print("\n"+"-"*10+" Manejo Soluciones "+"-"*10)
+print(f'Valor optimo: {vo}')
+        
+for h in h_:
+    for j in j_:
+        for i in i_:
+            if W[h, j, i].x != 0:
+                print(f'Cuadrante h={h} con cultivo i={i} se riega a través del método j={j}')
+                
+# ¿Cuál de las restricciones son activas?
+# print("\n"+"-"*9+" Restricciones Activas "+"-"*9)
+
+# for constr in model.getConstrs():
+#   print(constr, constr.getAttr('slack'))
+
+
